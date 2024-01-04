aae5a863d7817f761fa6f33a5ae0f34544757231
12vv <yegarrick12@gmail.com>
2024-01-04 17:31:54 +0800

    fix

--- src/components/loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress, CircularProgressLabel } from "@chakra-ui/core";
-
-const Loading = () => {
-    return (
-        <CircularProgress isIndeterminate color='black.300' />
-    );
-};
-
-export default Loading;
